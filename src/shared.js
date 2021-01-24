bcac7d9bfb76391c38ea7adf7e044071202dad29
Andrii Doroshenko <andry.dorosh@gmail.com>
2021-01-24 02:57:32 +0200

    fix error and add new game to bin section of package.json

--- src/shared.js
+++ src/shared.js
@@ -12,7 +12,7 @@ export function isEven(number) {
 }
 
 export function isPrime(number) {
-  for (let i = 2; i < number; i + 1) {
+  for (let i = 2; i < number; i += 1) {
     if (number % i === 0) return false;
   }
   return number > 1;
