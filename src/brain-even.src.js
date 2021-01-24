19f21e71986cdcc05946ae5325efaec7fa9be1a5
Andrii Doroshenko <andry.dorosh@gmail.com>
2021-01-24 04:31:55 +0200

    refactor - rename

--- src/brain-even.src.js
+++ src/brain-even.src.js
@@ -9,9 +9,9 @@ export default function brainEvenGame() {
   const taskDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
   const gameIterationCallback = () => {
-    const randomNumber = getRandomInt(24);
-    const correctAnswer = isEven(randomNumber);
-    return [randomNumber, YES_OR_NO_ANSWERS_MAP[correctAnswer]];
+    const possibleEvenNumber = getRandomInt(24);
+    const correctAnswer = isEven(possibleEvenNumber);
+    return [possibleEvenNumber, YES_OR_NO_ANSWERS_MAP[correctAnswer]];
   };
 
   playGame(taskDescription, gameIterationCallback);
--- src/brain-prime.src.js
+++ src/brain-prime.src.js
@@ -9,9 +9,9 @@ export default function brainPrimeGame() {
   const taskDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   const gameIterationCallback = () => {
-    const randomNumber = getRandomInt(24);
-    const correctAnswer = isPrime(randomNumber);
-    return [randomNumber, YES_OR_NO_ANSWERS_MAP[correctAnswer]];
+    const possiblePrimeNumber = getRandomInt(24);
+    const correctAnswer = isPrime(possiblePrimeNumber);
+    return [possiblePrimeNumber, YES_OR_NO_ANSWERS_MAP[correctAnswer]];
   };
 
   playGame(taskDescription, gameIterationCallback);
